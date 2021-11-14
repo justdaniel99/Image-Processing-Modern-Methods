ba09cf5952413d32dbf5a78bf73c18dfa5fd9e45
Daniil Andrianov <70854465+justdaniel99@users.noreply.github.com>
2021-11-14 12:57:51 +0300

    Vessels Detection function is added

--- Task3.py
+++ Task3.py
@@ -180,8 +180,109 @@ def canny(params, input_image, output_image):
     image_file_save(output_image, res.astype('uint8'))
 
 
+# vessels [for chosen sigma]
+def vessels_sigma(img, sigma):
+    convolvexx = np.zeros((img.shape[0], img.shape[1], 3))
+    convolvexy = np.zeros((img.shape[0], img.shape[1], 3))
+    convolveyy = np.zeros((img.shape[0], img.shape[1], 3))
+    G = np.zeros((img.shape[0], img.shape[1], 3))
+    Z = np.zeros((img.shape[0], img.shape[1], 3))
+    theta = np.zeros((img.shape[0], img.shape[1], 3))
+
+    size = 2 * round(3 * sigma) + 1
+
+    x = np.arange(- (size - 1) // 2, (size + 1) // 2)
+    y = np.arange(- (size - 1) // 2, (size + 1) // 2).reshape(-1, 1)
+
+    gxx = np.exp(- (x ** 2 + y ** 2) / (2 * sigma ** 2)) * (- 1 / sigma ** 2 + x ** 2 / sigma ** 4)
+    gxy = np.exp(- (x ** 2 + y ** 2) / (2 * sigma ** 2)) * (x * y / sigma ** 4)
+    gyy = np.exp(- (x ** 2 + y ** 2) / (2 * sigma ** 2)) * (- 1 / sigma ** 2 + y ** 2 / sigma ** 4)
+
+    convolvexx[:, :, 0] = ndimage.convolve(img[:, :, 0], gxx)
+    convolvexy[:, :, 0] = ndimage.convolve(img[:, :, 0], gxy)
+    convolveyy[:, :, 0] = ndimage.convolve(img[:, :, 0], gyy)
+
+    for i in range(1, img.shape[0] - 1):
+        for j in range(1, img.shape[1] - 1):
+
+            hessian = np.block([[np.sum(convolvexx[i - 1:i + 1, j - 1:j + 1, 0]),
+                                 np.sum(convolvexy[i - 1:i + 1, j - 1:j + 1, 0])],
+                                [np.sum(convolvexy[i - 1:i + 1, j - 1:j + 1, 0]),
+                                 np.sum(convolveyy[i - 1:i + 1, j - 1:j + 1, 0])]])
+
+            eigenvalues, eigenvectors = np.linalg.eig(hessian)
+            abs_eigenvalues = np.abs(eigenvalues)
+            index = np.argmax(abs_eigenvalues)
+
+            theta[i, j, 0] = np.arctan2(eigenvectors[index][1], eigenvectors[index][0]) * 180 / np.pi
+
+            if eigenvalues[index] > 0:
+                G[i, j, 0] = eigenvalues[index]
+            else:
+                G[i, j, 0] = 0
+
+    G = G / G.max() * 255
+    theta[theta < 0] += 180
+
+    for i in range(1, G.shape[0] - 1):
+        for j in range(1, G.shape[1] - 1):
+
+            try:
+                q = 255
+                r = 255
+
+                # angle 0
+                if (0.0 <= theta[i, j, 0] < 22.5) or (157.5 <= theta[i, j, 0] <= 180.0):
+                    q = G[i, j + 1, 0]
+                    r = G[i, j - 1, 0]
+
+                # angle 45
+                elif 22.5 <= theta[i, j, 0] < 67.5:
+                    q = G[i + 1, j - 1, 0]
+                    r = G[i - 1, j + 1, 0]
+
+                # angle 90
+                elif 67.5 <= theta[i, j, 0] < 112.5:
+                    q = G[i + 1, j, 0]
+                    r = G[i - 1, j, 0]
+
+                # angle 135
+                elif 112.5 <= theta[i, j, 0] < 157.5:
+                    q = G[i - 1, j - 1, 0]
+                    r = G[i + 1, j + 1, 0]
+
+                if (G[i, j, 0] >= q) and (G[i, j, 0] >= r):
+                    Z[i, j, 0] = G[i, j, 0]
+                else:
+                    Z[i, j, 0] = 0
+
+            except IndexError:
+                pass
+
+    for i in range(3):
+        Z[:, :, i] = Z[:, :, 0]
+
+    return Z
+
+
+# vessels [combined function]
 def vessels(input_image, output_image):
-    return 0  # To Do!
+    img = image_file_read(input_image).astype(float)
+
+    final = np.zeros((img.shape[0], img.shape[1], 3))
+
+    img_sigma_2 = vessels_sigma(img, 2)
+    img_sigma_3 = vessels_sigma(img, 3)
+    img_sigma_4 = vessels_sigma(img, 4)
+
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            final[i, j, 0] = max(img_sigma_2[i, j, 0], img_sigma_3[i, j, 0], img_sigma_4[i, j, 0])
+
+    for k in range(3):
+        final[:, :, k] = final[:, :, 0]
+
+    image_file_save(output_image, final.astype('uint8'))
 
 
 if __name__ == '__main__':
@@ -195,3 +296,4 @@ if __name__ == '__main__':
         canny(namespace.params, namespace.input_file[0], namespace.output_file[0])
     else:
         vessels(namespace.input_file[0], namespace.output_file[0])
+
